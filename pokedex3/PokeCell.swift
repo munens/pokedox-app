e75375ed2071a5d10c569886530d8af91e60767c
munens <kaumbuthom@yahoo.com>
2017-05-04 15:35:57 -0700

    Sec. 5, lec. 90: collection view delegate, data source, flow layout

--- pokedex3/PokeCell.swift
+++ pokedex3/PokeCell.swift
@@ -15,6 +15,14 @@ class PokeCell: UICollectionViewCell {
     
     var pokemon: Pokemon!
     
+    // let us round the corners of each of the collction view cells:
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    
     func configureCell(pokemon: Pokemon){
         self.pokemon = pokemon
         
--- pokedex3/ViewController.swift
+++ pokedex3/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    // tie the collection view to this class:
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
+    // UICollectionViewDelegateFlowLayout creates the settings for the layout of the collection view.
+    // UICollectionViewDataSource hodls the data for the collection view.
+    // UICollectionViewDelegate sets this class as the delegate for the collection view.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        collection.dataSource = self
+        collection.delegate = self
     }
-
+    /*
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }*/
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // deqeueueReusableCell below recycles the cells that we see in the collection view: - we dont want to have to download all of them at once.
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            let pokemon = Pokemon(name: "Pokemon", pokedexId: indexPath.row)
+            cell.configureCell(pokemon: pokemon)
+            
+            return cell
+        }
+        return UICollectionViewCell()
     }
-
+    
+    // function that runs when an item in the collection view is selected:
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    // this next function returns the no of sections in the collection view:
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    // find the no. of sections in our collection view:
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    // sets the height of each collection view cell:
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 100, height: 100)
+    }
 
 }
 
